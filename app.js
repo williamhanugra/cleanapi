f6d327a3bf3333ca5ea6f6c90a7f5161731434f3
Naufal Farras <muhammadnaufalfm@gmail.com>
2017-07-20 15:09:41 +0700

    connection.js dan app.js diubah

--- app.js
+++ app.js
@@ -1,46 +1,63 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-var connection = require('./connection');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('./socket/socket-io').listen(server);
-var port = process.env.PORT || 2016;
+// var express = require('express');
+// var bodyparser = require('body-parser');
+var sequelize = require('./connection').sequelize;
+var Sequelize = require('sequelize');
+// var app = express();
+// var server = require('http').createServer(app);
+// var io = require('./socket/socket-io').listen(server);
+// var port = process.env.PORT || 2016;
 
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-});
+// server.listen(port, function () {
+//   console.log('Server listening at port %d', port);
+// });
 
-var perangkatRoutes = require('./routes/perangkat');
-var dataRoutes = require('./routes/data');
-var batteryRoutes = require('./routes/battery');
-var userRoutes = require('./routes/user');
-var tdlRoutes = require('./routes/tdl');
-var statusRoutes = require('./routes/status');
-var safeRoutes = require('./routes/safe');
+const Status = sequelize.define('status_perangkat', {
+  id_status: {
+    type: Sequelize.INTEGER, 
+    primaryKey: true
+  }, 
+  port_perangkat: Sequelize.INTEGER,
+  id_user: Sequelize.INTEGER,
+  status: Sequelize.INTEGER
+}, {
+  timestamps: false,
+})
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+Status.findAll({where:{port_perangkat:31}},{raw: true}).then(stat => {
+  console.log(JSON.parse(JSON.stringify(stat))[0]["id_status"])
+})
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyparser.urlencoded({extended: true}));
-app.use(bodyparser.json());
+// var perangkatRoutes = require('./routes/perangkat');
+// var dataRoutes = require('./routes/data');
+// var batteryRoutes = require('./routes/battery');
+// var userRoutes = require('./routes/user');
+// var tdlRoutes = require('./routes/tdl');
+// var statusRoutes = require('./routes/status');
+// var safeRoutes = require('./routes/safe');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
-connection.init();
+// app.use(express.static(__dirname + '/public'));
+// app.use(bodyparser.urlencoded({extended: true}));
+// app.use(bodyparser.json());
 
-// ROUTES
-perangkatRoutes.configure(app);
-dataRoutes.configure(app);
-batteryRoutes.configure(app);
-userRoutes.configure(app);
-tdlRoutes.configure(app);
-statusRoutes.configure(app);
-safeRoutes.configure(app);
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+// connection.init();
+
+// // ROUTES
+// perangkatRoutes.configure(app);
+// dataRoutes.configure(app);
+// batteryRoutes.configure(app);
+// userRoutes.configure(app);
+// tdlRoutes.configure(app);
+// statusRoutes.configure(app);
+// safeRoutes.configure(app);
--- connection.js
+++ connection.js
@@ -1,22 +1,39 @@
 ﻿var mysql = require('mysql');
+var Sequelize = require('sequelize');
 
-function Connection() {
-  this.pool = null;
+// function Connection() {
+//   this.pool = null;
 
-  this.init = function() {
-    this.pool = mysql.createPool({
-      host: 'http://agrihack.party:32769/',
-      user: 'root',
-      password: 'sakura#2017',
-      database: 'sakura2017'
-    });
-  };
+//   this.init = function() {
+//     this.pool = mysql.createPool({
+//       host: 'http://agrihack.party:32769/',
+//       user: 'root',
+//       password: 'sakura#2017',
+//       database: 'sakura2017'
+//     });
+//   };
 
-  this.acquire = function(callback) {
-    this.pool.getConnection(function(err, connection) {
-      callback(err, connection);
-    });
-  };
-}
+//   this.acquire = function(callback) {
+//     this.pool.getConnection(function(err, connection) {
+//       callback(err, connection);
+//     });
+//   };
+// }
 
-module.exports = new Connection();
+var sequelize = new Sequelize('sakuratest','root','sakura#2017', {
+  host: 'agrihack.party',
+  port: '32769',
+  dialect: 'mysql',
+  pool : {
+    max: 5,
+    min: 0,
+    idle: 10000
+  } 
+});
+
+sequelize.authenticate().then(() => {console.log('Connection has been established successfully.');})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+module.exports.sequelize = sequelize;
